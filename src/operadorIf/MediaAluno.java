06dd1eda5db6847936bc63f32a315a0f4e69fc4c
Gilberto Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-01-24 01:45:25 -0300

    Estudo If

--- src/operadorIf/MediaAluno.java
+++ src/operadorIf/MediaAluno.java
@@ -3,26 +3,28 @@ package operadorIf;
 import java.util.Scanner;
 
 public class MediaAluno {
-	
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Informe a média:");
 		double media = input.nextDouble();
-		
-		if(media <= 10 && media >= 7) {
+
+		if (media <= 10 && media >= 7) {
 			System.out.println("Aprovado!");
 			System.out.println("Parabéns!!");
 		}
-		
-		if(media < 7 && media >= 4.5) {
+
+		else if (media < 7 && media >= 4.5) {
 			System.out.println("Recuperação!!!");
 		}
-		
-		if(media < 4.5 && media >= 0) {
+
+		else if (media < 4.5 && media >= 0) {
 			System.out.println("Reprovado!!!");
+		} else {
+			System.out.println("Error!!!");
 		}
-		
+
 		input.close();
 	}
 
